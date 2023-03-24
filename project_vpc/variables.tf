6607e1bb7a2aef1936d64beb5038c3e4896e5a3e
Murugan <thangadurai.murugan@tesco.com>
2023-03-24 11:50:22 +0530

    identation fixed

--- project_vpc/variables.tf
+++ project_vpc/variables.tf
@@ -1,9 +1,8 @@
-variable "region"{
+variable "region" {
   default = "ap-south-1"
-  type = string
+  type    = string
 }
-variable "cidr_block"{
+variable "cidr_block" {
   default = "192.168.0.0/16"
-  type = string
+  type    = string
 }
-
