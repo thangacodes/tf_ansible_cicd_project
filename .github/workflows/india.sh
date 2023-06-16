79805432f927c1a1d67f1620e8fbb35d694df4a2
Murugan <thangadurai.murugan@tesco.com>
2023-06-16 12:59:42 +0530

    removed unwanted files

--- .github/workflows/india.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "India is my country"
--- demo/s3_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_version = ">= 0.12.24"
-  
-  backend "s3" {
-    bucket = "gitops-demo-bucket-tf"
-    key = "gitops.tfstate"
-    region = "ap-south-1"
-    }
-}
