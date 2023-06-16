83ee31bec780c2d263b5d00e5bcf4d2274aaed72
Thangadurai M <38905243+thangacodes@users.noreply.github.com>
2023-06-16 17:54:25 +0530

    Update main.tf

--- demo/main.tf
+++ demo/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo" {
-  ami                    = ami-0e6329e222e662a52
-  instance_type          = t2.micro
-  vpc_security_group_ids = sg-0fb1052b659369aa8
-  key_name               = admin
+  ami                    = "ami-0e6329e222e662a52"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0fb1052b659369aa8"]
+  key_name               = "admin"
   count                  = 1
-  tags                   = {
+  tags = {
     Name = "GitOps_EC2"
-}
+  }
 }
